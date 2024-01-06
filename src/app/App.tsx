81a65c91d587123cd7c6924429db04c3593f6683
kornienkoas <work.kornienko@yandex.ru>
2024-01-06 21:16:41 +0300

    add routs

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,8 +1,28 @@
+import { Typography } from "antd";
 import React from "react";
+import { Route, Routes } from "react-router";
+import { Link } from "react-router-dom";
 import MovieList from "../components/MovieList";
 
 function App() {
-  return <MovieList />;
+  return (
+    <>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/movies">movies</Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Typography.Title>Home</Typography.Title>} />
+        <Route path="/movies" element={<MovieList />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
 
 import App from "./app/App";
 import { setupStore } from "./store/store";
@@ -13,7 +14,9 @@ const root = ReactDOM.createRoot(
 root.render(
   <Provider store={store}>
     <React.StrictMode>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </React.StrictMode>
   </Provider>
 );
