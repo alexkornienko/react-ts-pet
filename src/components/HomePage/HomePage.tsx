f84fdb15efa951fe950e78523780661a8207dd78
kornienkoas <work.kornienko@yandex.ru>
2024-10-01 21:59:07 +0300

    review

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -1,15 +1,13 @@
 import React from "react";
-import { Flex, Space, Typography } from "antd";
+import { Typography } from "antd";
 import NowAtCinema from "./NowAtCinema";
 
 const HomePage = () => {
   return (
-    // <Flex vertical>
     <>
       <Typography.Title>Home Page</Typography.Title>
       <NowAtCinema />
     </>
-    // </Flex>
   );
 };
 
--- src/components/HomePage/NowAtCinema.tsx
+++ src/components/HomePage/NowAtCinema.tsx
@@ -3,6 +3,7 @@ import { useNavigate } from "react-router-dom";
 
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Pagination, Scrollbar, A11y } from "swiper/modules";
+import { createStyles } from "antd-style";
 
 import { baseImageUrl, useGetNowPlayingQuery } from "../../services/movieApi";
 import { IMovieCard } from "../../types/movieCard";
@@ -14,10 +15,51 @@ import "swiper/css/pagination";
 import "swiper/css/scrollbar";
 import "swiper/swiper-bundle.css";
 
+const useStyles = createStyles({
+  container: {
+    display: "flex",
+    flexDirection: "column",
+    gap: "6px",
+    marginBottom: "40px",
+    position: "relative",
+    boxShadow: "0px 4px 6px rgba(0, 0, 0, 0.1)",
+    color: "white",
+    borderRadius: "10px",
+    padding: "6px 8px",
+    height: "350px",
+    width: "215px",
+    overflow: "hidden",
+    cursor: "pointer",
+  },
+  backgroundImage: {
+    position: "absolute",
+    top: 0,
+    right: 0,
+    bottom: 0,
+    left: 0,
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+  },
+  contentWrapper: {
+    position: "relative",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "space-between",
+    height: "100%",
+  },
+  title: {
+    fontSize: "1.25rem",
+    textShadow: "2px 2px 6px rgba(0, 0, 0, 0.4)",
+    fontWeight: 500,
+  },
+  average: { alignSelf: "end" },
+});
+
 const NowAtCinema = () => {
+  const { styles } = useStyles();
   const navigate = useNavigate();
 
-  const { data, isLoading, error } = useGetNowPlayingQuery();
+  const { data } = useGetNowPlayingQuery();
 
   const handleGetMoviePage = (id: number) => {
     navigate(`/movies/${id}`);
@@ -35,54 +77,16 @@ const NowAtCinema = () => {
           key={movie.id}
           onClick={handleGetMoviePage.bind(this, movie.id)}
         >
-          <div
-            style={{
-              display: "flex",
-              flexDirection: "column",
-              gap: "6px",
-              marginBottom: "40px",
-              position: "relative",
-              boxShadow: "0px 4px 6px rgba(0, 0, 0, 0.1)",
-              color: "white",
-              borderRadius: "10px",
-              padding: "6px 8px",
-              height: "350px",
-              width: "215px",
-              overflow: "hidden",
-              cursor: "pointer",
-            }}
-          >
+          <div className={styles.container}>
             <div
+              className={styles.backgroundImage}
               style={{
                 backgroundImage: `url(${baseImageUrl}${movie.backdrop_path})`,
-                position: "absolute",
-                top: 0,
-                right: 0,
-                bottom: 0,
-                left: 0,
-                backgroundSize: "cover",
-                backgroundPosition: "center",
               }}
             />
-            <div
-              style={{
-                position: "relative",
-                display: "flex",
-                flexDirection: "column",
-                justifyContent: "space-between",
-                height: "100%",
-              }}
-            >
-              <h1
-                style={{
-                  fontSize: "1.25rem",
-                  textShadow: "2px 2px 6px rgba(0, 0, 0, 0.4)",
-                  fontWeight: 500,
-                }}
-              >
-                {movie.title}
-              </h1>
-              <div style={{ alignSelf: "end" }}>
+            <div className={styles.contentWrapper}>
+              <h1 className={styles.title}>{movie.title}</h1>
+              <div className={styles.average}>
                 <Average average={movie.vote_average} sizeSmall />
               </div>
             </div>
